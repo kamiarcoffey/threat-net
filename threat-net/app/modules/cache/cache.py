5174f3e71c96e362668d8d8446074fcf4474ecf2
Alex Burnley <albu5828@colorado.edu>
2020-02-20 12:12:26 -0700

    Added Cache Module and made thread-safe

--- threat-net/app/modules/cache/cache.py
+++ threat-net/app/modules/cache/cache.py
@@ -1,20 +1,26 @@
 from cachetools import TTLCache
-import time
+
 import json
-import unittest
+from cacheConfig import * 
+from threading import Lock
+
 
 
 class Cache:
+
+	
 
 	# Initializes a cache object based on a config file
 	def __init__(self):
 		with open('../config.json') as json_data_file:
 			data = json.load(json_data_file)
 		self.cache = TTLCache(data["cache"]["maxItems"], data["cache"]["timeToLive"])
+		self.lock = Lock()
 
 	# Initializes TTL cache based on number of items and time to live 
 	def __init__(self,numItems,timeToLive):
 		self.cache = TTLCache(numItems,timeToLive)
+		self.lock = Lock()
 
 	# returns a pair, (bool success, value)
 	# if it was a cache hit, success will be true, otherwise it will be false
@@ -27,43 +33,10 @@ class Cache:
 
 	# adds the key, value pair to the cache
 	def addToCache(self,key, value):
+		self.lock.acquire()
 		self.cache[key] = value
+		self.lock.release()
 
-
-
-
-class TestCache(unittest.TestCase):
-	def setUp(self):
-		pass
-	
-	def testNotInCache(self):
-		cache1 = Cache(3,1)
-		self.assertEqual(cache1.checkCache(1),(False,None))
-
-	def testInCache(self):
-		cache1 = Cache(3,1)
-		cache1.addToCache(1,12)
-		self.assertEqual(cache1.checkCache(1),(True, 12))
-
-	def testCacheExpire(self):
-		cache1 = Cache(3,1)
-		cache1.addToCache(1,12)
-		time.sleep(2)
-		self.assertEqual(cache1.checkCache(1),(False,None))
-
-	def testCacheFull(self):
-		cache1 = Cache(3,1)
-		cache1.addToCache(1,12)
-		cache1.addToCache(2,13)
-		cache1.addToCache(3,13)
-		cache1.addToCache(4,13)
-
-		self.assertEqual(cache1.checkCache(1),(False,None))
-
-
-
-if __name__ == '__main__':
-	unittest.main()
 
 
 
---
+++ threat-net/app/modules/cache/cacheTest.py
@@ -0,0 +1,35 @@
+import unittest
+import time
+from cache import *
+
+class TestCache(unittest.TestCase):
+	def setUp(self):
+		pass
+	
+	def testNotInCache(self):
+		cache1 = Cache(3,1)
+		self.assertEqual(cache1.checkCache(1),(False,None))
+
+	def testInCache(self):
+		cache1 = Cache(3,1)
+		cache1.addToCache(1,12)
+		self.assertEqual(cache1.checkCache(1),(True, 12))
+
+	def testCacheExpire(self):
+		cache1 = Cache(3,1)
+		cache1.addToCache(1,12)
+		time.sleep(2)
+		self.assertEqual(cache1.checkCache(1),(False,None))
+
+	def testCacheFull(self):
+		cache1 = Cache(3,1)
+		cache1.addToCache(1,12)
+		cache1.addToCache(2,13)
+		cache1.addToCache(3,13)
+		cache1.addToCache(4,13)
+
+		self.assertEqual(cache1.checkCache(1),(False,None))
+
+if __name__ == '__main__':
+	unittest.main()
+

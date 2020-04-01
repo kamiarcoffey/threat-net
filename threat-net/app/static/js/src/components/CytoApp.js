45ffc86a862e4c5b35b2ea4e2a806de98a00ae67
Connor Guerin <cogu4579@colorado.edu>
2020-03-31 22:13:56 -0600

    Merge html structure and demo Cytoscape graph

--- threat-net/app/static/js/src/components/CytoApp.js
+++ threat-net/app/static/js/src/components/CytoApp.js
@@ -8,14 +8,7 @@ class CytoApp extends React.Component {
 
     render(){
         return (
-            <div>
-                <Header />
-                <div className = "CytoContainer">
-                    <div className = "nodeInfo">What goes here?</div>
-                    <Cytoscape />
-                    <div className = "nodeFilter">Filter?</div>
-                </div>
-            </div>
+            <Cytoscape />
         )
     };
 
--- threat-net/app/static/js/src/components/Cytoscape.js
+++ threat-net/app/static/js/src/components/Cytoscape.js
@@ -1,30 +1,74 @@
 import React from 'react'
 import CytoscapeComponent from 'react-cytoscapejs';
+// import nodeHtmlLabel from 'cytoscape-node-html-label';
+
 
 let elements = [
-  { data: { id: 'one', label: 'Node 1' }, position: { x: 300, y: 300 } },
-  { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 400 } },
-  { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+    { data: { id: 'ioc1', label: 'IOC 1' }, position: { x: 150, y: 400 } },
+    { data: { id: 'ioc2', label: 'IOC 2' }, position: { x: 250, y: 50  } },
+    { data: { id: 'ioc3', label: 'IOC 3' }, position: { x: 250, y: 200 } },
+    { data: { id: 'ioc4', label: 'IOC 4' }, position: { x: 350, y: 200 } },
+    { data: { id: 'ioc5', label: 'IOC 5' }, position: { x: 150, y: 300 } },
+    { data: { id: 'ioc6', label: 'IOC 6' }, position: { x: 250, y: 300 } },
+    { data: { id: 'ioc7', label: 'IOC 7' }, position: { x: 350, y: 400 } },
+
+
+    {
+      data: { source: 'ioc1', target: 'ioc2', label: '1 -> 2' }
+    },
+    {
+      data: { source: 'ioc1', target: 'ioc3', label: '1 -> 3' }
+    },
+    {
+      data: { source: 'ioc1', target: 'ioc4', label: '1 -> 4' }
+    },
+    {
+      data: { source: 'ioc1', target: 'ioc5', label: '1 -> 5' }
+    },
+    {
+      data: { source: 'ioc1', target: 'ioc6', label: '1 -> 6' }
+    },
+    {
+      data: { source: 'ioc1', target: 'ioc7', label: '1 -> 7' }
+    },
+    {
+      data: { source: 'ioc2', target: 'ioc5', label: '2 -> 5' }
+    },
+    {
+      data: { source: 'ioc3', target: 'ioc4', label: '3 -> 4' }
+    },
 ];
 
 let cy = <CytoscapeComponent className = "CytoscapeGraph"
 stylesheet={[
     {
-    selector: 'node',
-    style: {
-        width: 30,
-        height: 20,
-        shape: 'square'
-    }
+    layout: {
+      name: 'random'
     },
-    {
-    selector: 'edge',
-    style: {
-        width: 5
+   selector: 'node',
+   style: {
+     // 'background-color': 'data(color)',
+     'label': 'data(label)',
+     'font-size' : '25px',
+     // 'width' : 'data(size)',
+     // 'height' : 'data(size)',
+    },
+    // },
+    // {
+    // selector: 'edge',
+    // style: {
+    //     width: 5
+    // },
+    selector: ':selected',
+      css: {
+        'background-color': 'black',
+        'line-color': 'black',
+        'target-arrow-color': 'black',
+        'source-arrow-color': 'black'
+      }
     }
-    }
-]}  
-elements={elements} style={ { width: '500px', height: '500px' } }
+]}
+elements={elements} style={ { width: 'calc(100vw - 400px)', height: 'calc(100vh - 50px)' } }
 />
 
 class Cytoscape extends React.Component {
@@ -33,12 +77,6 @@ class Cytoscape extends React.Component {
     }
   
     render(){
-      // const elements = [
-      //    { data: { id: 'one', label: 'Node 1' }, position: { x: 300, y: 300 } },
-      //    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 400 } },
-      //    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-      // ];
-  
       return (
         <div className = "CytoContainer">
             {cy}
@@ -61,7 +99,9 @@ const AddNode = (object = {}) => {
   elements.push(object);
 }
 
+
+
 console.log(FindNode('one'));
-AddNode({ data: { id: 'three', label: 'Node 3' }, position: { x: 250, y: 300 } });
+//AddNode({ data: { id: 'three', label: 'Node 3' }, position: { x: 250, y: 300 } });
 
   export default Cytoscape;

d43a8b28c9054765a4e6e6ce9981692455a27997
Connor Guerin <cogu4579@colorado.edu>
2020-04-22 11:20:01 -0600

    Add ajax call for search bar to filesystem collection

--- threat-net/app/static/js/src/components/SearchBar.js
+++ threat-net/app/static/js/src/components/SearchBar.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import {toast} from "react-toastify";
 
 class SearchBar extends Component {
     constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+        value: '',
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    setData = data => {
+        this.props.setData(data);
+    };
 
     handleChange(event) {
     this.setState({value: event.target.value});
@@ -18,11 +25,27 @@ class SearchBar extends Component {
     event.preventDefault();
     }
 
+    querySHA = () => {
+        $.ajax({
+            type : "GET",
+            url : `/API/IOC/queryDetails/fs/${this.state.value}`,
+            context: this,
+            success: function (data) {
+                this.setData(data);
+                toast.success(`Node with SHA ${this.state.value} successfully loaded.`);
+                },
+            error: function(response) {
+                toast.error(`Unable to load node with SHA ${this.state.value}. Error: ${response}`)
+                },
+            },
+        );
+    };
+
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.querySHA}>
                 <input id="search-bar" type="text" className="search-bar"  value={this.state.value} onChange={this.handleChange} placeholder="Search..." />
-                <button id="search-button" value="Submit" type="submit" onClick={this.loadGraph}>
+                <button id="search-button" value="Submit" type="submit" onClick={this.querySHA}>
                     Search
                 </button>
             </form>
